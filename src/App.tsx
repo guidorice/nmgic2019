56b20cb59ed34778dd1c6e64b1241fe419b766e7
Alex G Rice <rice.guido@gmail.com>
2019-10-30 16:27:09 +0000

    Move mapbox-gl.css into app bundle. Remove mapbox-gl.js from index.html.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Map from './Map';
 import Legend from './Legend';
+import '../node_modules/mapbox-gl/dist/mapbox-gl.css';
 
 const App: React.FC = () => {
   return (
